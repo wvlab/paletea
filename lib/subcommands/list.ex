32827f21aa28e856664c7b8ccf60f890c0cc2c1f
WVlab <tabs3536@gmail.com>
2023-05-20 21:30:30 +0300

    basic list subcommand implementation

--- lib/subcommands/list.ex
+++ lib/subcommands/list.ex
@@ -21,21 +21,61 @@ defmodule Paletea.Subcommands.List do
             parser: :string
           ]
         ],
-        options: [
-          format: [
-            short: "-f",
-            long: "--format",
-            help: "Format in which data will be printed",
-            default: "plain",
-            parser: :string
-          ]
-        ]
+        #options: [
+        #  format: [
+        #    short: "-f",
+        #    long: "--format",
+        #    help: "Format in which data will be printed",
+        #    default: "plain",
+        #    parser: :string
+        #  ]
+        #]
       ]
     ]
   end
 
   @impl Paletea.Subcommand
-  def run(args) do
-    args
+  def run(%{args: args, options: _opts}) do
+    case args do
+      %{category: "themes", subcategory: nil} ->
+        themes()
+      %{category: "colors", subcategory: theme} when not is_nil(theme) ->
+        colors(theme)
+      _ ->
+        IO.warn("Can't find that category or subcategory")
+    end
+  end
+
+  defp print_list(enumerable, offset \\ 1) do
+    enumerable
+    |> Enum.with_index(offset)
+    |> Enum.each(fn
+      {theme, index} -> IO.puts([to_string(index), ". ", theme])
+    end)
+  end
+
+  defp themes() do
+    themes_dir = XDG.get_data_path()
+
+    case File.ls(themes_dir) do
+      {:ok, list} ->
+        list
+        |> Enum.filter(&File.exists?(Path.join(themes_dir, &1)))
+        |> print_list()
+      {:error, reason} ->
+        IO.puts(reason)
+    end
+  end
+  
+  defp colors(theme) do
+    path = Path.join([XDG.get_data_path(), theme, "theme.toml"]) 
+    
+    %{
+      "colors" => colors
+    } = Toml.decode_file!(path)
+ 
+    colors
+    |> Enum.map(&elem(&1, 1))
+    |> print_list(0)
   end
 end
